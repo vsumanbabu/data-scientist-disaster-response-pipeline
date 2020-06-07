63320cfeff57edfd208fa639fef199d83774d8ba
Suman Veeramalla <vsumanbabu@gmail.com>
2020-06-07 19:56:06 -0400

    chore: Retrain Classifer model

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,39 +1,95 @@
+# import libraries
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report, accuracy_score
+import nltk
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+nltk.download(['punkt','wordnet'])
+import re
+import pickle
+from bs4 import BeautifulSoup
 
 def load_data(database_filepath):
-    pass
+    '''
+    '''
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('response',engine)
+    X = df['message']
+    Y = df.iloc[:,5:]
+    Y.fillna(0,inplace=True)
+    category_names = category_names=list(Y.columns)
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    '''
+    '''
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens=tokenizer.tokenize(text)
+    return tokens
 
 
 def build_model():
-    pass
+    '''
+    '''
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier())),
+    ])
+
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+
+    '''
+    Y_pred = model.predict(X_test)
+    category_names=list(Y.columns)
+    # Calculate the accuracy for each of them.
+    for i in range(len(category_names)):
+        print("Category:", category_names[i],"\n", classification_report(Y_test.iloc[:, i].values, Y_pred[:, i]))
+        print('Accuracy of %25s: %.2f' %(category_names[i], accuracy_score(Y_test.iloc[:, i].values, Y_pred[:,i])))
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    model_filepath='../models/classifier.pkl'
+    '''
+    pickle.dump(model, open(model_filepath, "wb"))
 
 
 def main():
+    '''
+    python \
+    models/train_classifier.py \
+    data/DisasterResponse.db \
+    models/classifier.pkl
+    '''
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
+        np.any(np.isnan(Y))
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
+        model = []
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
