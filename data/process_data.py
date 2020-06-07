d248dacfceb9188029411b47560cb6526df5bff7
Suman Veeramalla <vsumanbabu@gmail.com>
2020-06-07 01:15:29 -0400

    Add: Add ETL Pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,20 +1,65 @@
+# import libraries
 import sys
 import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    messages_filepath='../data/disaster_messages.csv'
+    categories_filepath='../data/disaster_categories.csv'
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df_merge_message_categories = pd.merge(messages,categories,on='id')
+    return df_merge_message_categories
 
 
 def clean_data(df):
-    pass
+    '''
+    '''
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';',expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x:x[:-2])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    #Convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(np.int)
+    # drop the original categories column from `df`
+    df.drop('categories',axis=1)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1)
+    # drop duplicates
+    df = df.drop_duplicates()
+    return df
 
 
 def save_data(df, database_filename):
-    pass
+    '''
+    database_filename=''sqlite:///../data/disaster.db''
+    '''
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql('response', engine, index=False)
 
 
 def main():
+    '''
+    python  data/process_data.py         \
+            data/disaster_messages.csv   \
+            data/disaster_categories.csv \
+            data/DisasterResponse.db
+    '''
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
